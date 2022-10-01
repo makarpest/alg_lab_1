ec9ad186ad1dc4435d34ff763b38020672d8e71e
makarpest <makarpestrikov04@gmail.com>
2022-10-02 00:00:14 +0300

    done?

--- sec.rs
+++ sec.rs
@@ -1,31 +1,3 @@
-use std::io::stdin;
-
-fn main() {
-    // входныке данные
-    let mut inp = String::new();
-    stdin().read_line(&mut inp).expect(" error ");
-    let mut input = String::new();
-    stdin().read_line(&mut input).expect(" error ");
-    let mut vec: Vec<i32> = input
-        .trim().split(' ')
-        .map(|s| s.parse().unwrap())
-        .collect();
-    // алгоритм
-    for i in 1..vec.len() {
-        let mut gr = i - 1;
-        let key = vec[i];
-        while gr > 0 && vec[gr] > key {
-            vec[gr + 1] = vec[gr];
-            gr -= 1;
-        }
-        vec[gr + 1] = key;
-        if gr == 0 && vec[0] > vec[1] {
-            let helper = vec[1];
-            vec[1] = vec[0];
-            vec[0] = helper;
-        }
-    }
-    for elem in vec {
-        print!("{elem} ");
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:dfcb840692ebf4b79fd315bc29536bf5c71b6d590311cf19e9a927877b1f9def
+size 803
